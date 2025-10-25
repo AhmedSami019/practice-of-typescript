ddd9a47cdd10063815f81a2d6de8fdd34a80e86c
ahmedsami019 <easami019192@gmail.com>
2025-10-25 20:09:51 +0600

    first commit

---
+++ console/script.js
@@ -0,0 +1,10 @@
+const country = "bangladesh";
+const my_speech = "just stay with e a sami";
+const myFunction = () => {
+    console.log("this text form the function");
+};
+console.log(country);
+console.log(my_speech);
+myFunction();
+export {};
+//# sourceMappingURL=script.js.map
