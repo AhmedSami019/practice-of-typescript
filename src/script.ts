6e5da36e8be06865ba0419a159b7e0377fc6c2c8
ahmedsami019 <easami019192@gmail.com>
2025-10-25 20:15:03 +0600

    cheking the type of variable by ts

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,6 @@
+
+// some code in basic
+
 const country = "bangladesh"
 const my_speech = "just stay with e a sami"
 
@@ -5,8 +8,18 @@ const myFunction = () => {
     console.log("this text form the function");
 }
 
-console.log(country);
-console.log(my_speech);
-myFunction()
+// console.log(country);
+// console.log(my_speech);
+// myFunction()
 
 
+const name = "emtiaz Ahmed sami"
+console.log(name);
+// name = 34
+// console.log(name);
+
+let text ;
+text = "how are you"
+console.log(text);
+text = 27;
+console.log(text);
