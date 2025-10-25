0da75dc1175586f51011164b5d636bc97b875d6f
ahmedsami019 <easami019192@gmail.com>
2025-10-25 20:20:49 +0600

    cheking type for object

--- console/script.js
+++ console/script.js
@@ -8,13 +8,23 @@ const myFunction = () => {
 // console.log(my_speech);
 // myFunction()
 const name = "emtiaz Ahmed sami";
-console.log(name);
+// console.log(name);
 // name = 34
 // console.log(name);
 let text;
 text = "how are you";
-console.log(text);
+// console.log(text);
 text = 27;
-console.log(text);
+// console.log(text);
+const car = {
+    name: "honda x43z",
+    oilQuantity: 50,
+    isSports: true,
+};
+console.log(car.name);
+car.name = "toyota z39x";
+console.log(car.name);
+car.companyName = "toyota";
+console.log(car);
 export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,25 +1,36 @@
-
 // some code in basic
 
-const country = "bangladesh"
-const my_speech = "just stay with e a sami"
+const country = "bangladesh";
+const my_speech = "just stay with e a sami";
 
 const myFunction = () => {
-    console.log("this text form the function");
-}
+  console.log("this text form the function");
+};
 
 // console.log(country);
 // console.log(my_speech);
 // myFunction()
 
-
-const name = "emtiaz Ahmed sami"
-console.log(name);
+const name = "emtiaz Ahmed sami";
+// console.log(name);
 // name = 34
 // console.log(name);
 
-let text ;
-text = "how are you"
-console.log(text);
+let text;
+text = "how are you";
+// console.log(text);
 text = 27;
-console.log(text);
+// console.log(text);
+
+const car = {
+  name: "honda x43z",
+  oilQuantity: 50,
+  isSports: true,
+};
+
+console.log(car.name);
+car.name = "toyota z39x"
+console.log(car.name);
+
+car.companyName = "toyota"
+console.log(car);
